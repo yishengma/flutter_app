35fbe962cceff8e1f12dc8e6c0a8f5483495327f
yishengma <13104862372@163.com>
2020-05-09 16:40:19 +0800

    创建一个ListView

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,38 @@ class RandomWordsState extends State<RandomWords> {
 
   @override
   Widget build(BuildContext context) {
-    final worldPair = new WordPair.random();
-    return new Text(worldPair.asPascalCase);
+//    final worldPair = new WordPair.random();
+//    return new Text(worldPair.asPascalCase);
+
+  //在 Dart 语言中使用 _下划线开头，会强制变成私有的
+      final _suggestions = <WordPair>[];
+      final _biggerFont = const TextStyle(fontSize: 18.0);
+      Widget _buildRow(WordPair pair) {
+        return new ListTile(
+          title: new Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      }
+      Widget _buildSuggestions() {
+        return new ListView.builder(
+            padding: const EdgeInsets.all(16.0),
+            itemBuilder: (context,i){
+              if(i.isOdd)
+                return new Divider();
+               final index = i~/2;
+               if(index >= _suggestions.length) {
+                 _suggestions.addAll(generateWordPairs().take(10));
+               }
+               return _buildRow(_suggestions[index]);
+            });
+      }
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Startup Name Generator'),
+        ),
+        body: _buildSuggestions(),
+      );
   }
 }
