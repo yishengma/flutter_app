40cdea24ab0c1f0c7fb252fecf72a6ee72336bb5
yishengma <13104862372@163.com>
2020-05-09 16:15:17 +0800

    添加一个有状态的部件

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());//dart 语法 执行main 函数，=> 执行单个函数
 
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
+//    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -18,9 +18,25 @@ class MyApp extends StatelessWidget {
         ),
         body: new Center(
 //          child: new Text('Hello world'),
-        child: new Text(wordPair.asPascalCase),
+//        child: new Text(wordPair.asPascalCase),
+            child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+
+  @override
+  State createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+
+  @override
+  Widget build(BuildContext context) {
+    final worldPair = new WordPair.random();
+    return new Text(worldPair.asPascalCase);
   }
 }
