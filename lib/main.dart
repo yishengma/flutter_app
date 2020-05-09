ffc66ec36fbdd4eef2499d3d432e489576789e59
yishengma <13104862372@163.com>
2020-05-09 16:02:42 +0800

    pubspec管理Flutter的资源，在此文件添加外部包

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());//dart 语法 执行main 函数，=> 执行单个函数
 
@@ -8,6 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -15,7 +17,8 @@ class MyApp extends StatelessWidget {
           title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          child: new Text('Hello world'),
+//          child: new Text('Hello world'),
+        child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
