a6911d3b4685ba9e94120008416c69bc816eab5b
yishengma <13104862372@163.com>
2020-05-14 20:48:47 +0800

    flutter 使用ListView

--- lib/flutter_demo1.dart
+++ lib/flutter_demo1.dart
@@ -29,7 +29,7 @@ class NewHomeContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: <Widget>[
         NewHomeProductItem("Apple","MacBook","https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2656132749,3881506365&fm=26&gp=0.jpg"),
         NewHomeProductItem("Apple","MacBook1","https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2656132749,3881506365&fm=26&gp=0.jpg"),
