4f6a52395a62242f429f4900e84c4db77307109a
yishengma <13104862372@163.com>
2020-05-14 16:38:02 +0800

    dart Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());//dart 语法 执行main 函数，=> 执行单个函数
 
-//继承 Widget 应用本身也成为一个 widget
-//widget 的主要工作是提供一个 build 的方法来描述如何根据较低的 widget 来显示自己
-class MyApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    Widget titleSection = new Container(
-      padding: const EdgeInsets.all(32.0),
-      child: new Row(
-      children:[
-        new Expanded(child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            new Container(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: new Text(
-                'Oeschinen Lake Campground',
-                style: new TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            new Text(
-              'Kandersteg, Switzerland',
-              style: new TextStyle(
-                  color: Colors.grey[500],
-              ),
-            ),
-          ],
-        ),
-        ),
-        new Icon(Icons.star,
-          color: Colors.red[500],
-        ),
-        new Text('41'),
-      ],
+main() {
+  runApp(Center(//将 Text 居中显示
+    child: Text(
+      "Hello world",
+      textDirection: TextDirection.ltr,//dart 需要指定方向
+      style: TextStyle(
+          fontSize: 30,
+          color: Colors.orange
+      ),
     ),
-    );
-    return titleSection;
-  }
+  )
+  );
 }
