236e5db2980ef7246acbc9fda2173a7a10faa74c
yishengma <13104862372@163.com>
2020-05-14 17:03:42 +0800

    dart 代码重构

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,56 @@ import 'package:flutter/material.dart';
 
 main() {
   runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,//去掉 debug 标签
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("第一个 Flutter 程序"),
-          ),
-          body: Center(
-            child: Text(
-              "Hello world",
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.orange
-              ),
-            ),
-          ),
-        ),//脚手架用于快速搭建页面
-      )
+      NewApp()
   );
 }
+
+/**
+ * Widget
+ * 有状态的 Widget StatefulWidget
+ * 无状态的 Widget StatelessWidget
+ *
+ */
+class NewApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, //去掉 debug 标签
+      home: NewHomePage(), //脚手架用于快速搭建页面
+    );
+  }
+}
+
+class NewHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("第一个 Flutter 程序"),
+        ),
+        body: NewCenterBody()
+    );
+  }
+}
+
+
+class NewCenterBody extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Hello world",
+        style: TextStyle(
+            fontSize: 30,
+            color: Colors.orange
+        ),
+      ),
+    );
+  }
+}
+
+
+
