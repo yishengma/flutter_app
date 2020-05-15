b086161ccdf089f658e9b62b2e4d667d2e3c2563
yishengma <13104862372@163.com>
2020-05-15 15:44:28 +0800

    flutter image-icon

--- lib/flutter_demo4.dart
+++ lib/flutter_demo4.dart
@@ -22,3 +22,37 @@ class NewApp extends StatelessWidget {
     );
   }
 }
+
+
+class NewImageHolder extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    //图片缓存
+    //Flutter 或记录图片缓存的高度和宽度
+    //默认可以缓存 1000 图片，最大为 100 MB
+    return FadeInImage(
+      fadeOutDuration: Duration(microseconds: 1),
+      placeholder: AssetImage(""),
+      image: NetworkImage(""),
+    );
+  }
+}
+
+
+class NewIconHolder extends StatelessWidget {
+
+  //Icon 字体图标和图片图标
+  //字体图标矢量图
+  //字体图标可以设置颜色
+  //占空间小
+  @override
+  Widget build(BuildContext context) {
+//    return Icon(Icons.pets,size: 300,color: Colors.purple);
+//    return Icon(IconData(0xe91d, fontFamily: 'MaterialIcons'), size: 300);
+
+    //0xe91d -> unicode 编码
+    return Text(
+        "\ue91d", style: TextStyle(fontSize: 100, color: Colors.orange,fontFamily: 'MaterialIcons'));
+  }
+}
